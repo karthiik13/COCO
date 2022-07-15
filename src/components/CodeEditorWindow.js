1a69510895c8c5fa8a9909fce03a5c168550bef7
karthiik13 <karthiksudarsan@gmail.com>
2022-07-15 09:53:43 +0530

    Removed Foooter and installed node modules.

--- src/components/CodeEditorWindow.js
+++ src/components/CodeEditorWindow.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 import Editor from "@monaco-editor/react";
+import MonacoCollabExt from "@convergencelabs/monaco-collab-ext";
+
 
 const CodeEditorWindow = ({ onChange, language, code, theme }) => {
   const [value, setValue] = useState(code || "");
